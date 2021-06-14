f76354b956865570da0add20888f69672ab6beab
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-06-14 13:27:30 +0545

    Use for loop to render articles

--- index.php
+++ index.php
@@ -4,21 +4,18 @@
         <title>A Blog Application</title>
     </head>
     <body>
+
         <h1>Blog Title</h1>
         <p>This paragraph summarises what the blog is about.</p>
 
-        <h2>Article 1 title</h2>
-        <div>dd mm yyyy</div>
-        <p>A paragraph summarising article 1.</p>
-        <p>
-            <a href="#">Read more...</a>
-        </p>
+        <?php for($postId = 1; $postId < 3; $postId++): ?>
+            <h2>Article <?php echo $postId;?> title</h2>
+            <div>dd mm yyyy</div>
+            <p>A paragraph summarising article <?php echo $postId;?></p>
+            <p>
+                <a href="#">Read more...</a>
+            </p>
+        <?php endfor;?>
 
-        <h2>Article 2 title</h2>
-        <div>dd mm yyyy</div>
-        <p> A paragraph summarising article 2.</p>
-        <p>
-            <a href="#">Read more...</a>
-        </p>
     </body>
 </html>
