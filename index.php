b613c90842a97fa7907055117e8a80c2607c45fb
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-06-14 13:18:00 +0545

    Add initial skeleton home page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>A Blog Application</title>
+    </head>
+    <body>
+        <h1>Blog Title</h1>
+        <p>This paragraph summarises what the blog is about.</p>
+
+        <h2>Article 1 title</h2>
+        <div>dd mm yyyy</div>
+        <p>A paragraph summarising article 1.</p>
+        <p>
+            <a href="#">Read more...</a>
+        </p>
+
+        <h2>Article 2 title</h2>
+        <div>dd mm yyyy</div>
+        <p> A paragraph summarising article 2.</p>
+        <p>
+            <a href="#">Read more...</a>
+        </p>
+    </body>
+</html>
