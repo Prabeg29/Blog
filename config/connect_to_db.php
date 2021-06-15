db0b0a7811de93cab95023981d3ddf9b1fbff2a6
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-06-15 13:30:32 +0545

    Show all post from database

--- config/connect_to_db.php
+++ config/connect_to_db.php
@@ -5,9 +5,8 @@
     $password = 'test1234';
     $database = 'blog';
 
-
     // Create Connection
-    $conn = mysqli_connect($serverName, $username, $password, $database);
+    $conn = mysqli_connect($servername, $username, $password, $database);
 
     // Check Connection
     if($conn->connect_error){
--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+    require('./config/connect_to_db.php');
+
+    // Statement to get all posts
+    $sql = "SELECT id, title, body, created_at FROM post ORDER BY created_at DESC";
+
+    // Query the database and get posts
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } 
+    else {
+        echo "0 results";
+    }
+
+    // free result
+    mysqli_free_result($result);
+
+    // close connection
+    mysqli_close($conn);
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,14 +31,14 @@
         <h1>Blog Title</h1>
         <p>This paragraph summarises what the blog is about.</p>
 
-        <?php for($postId = 1; $postId < 3; $postId++): ?>
-            <h2>Article <?php echo $postId;?> title</h2>
-            <div>dd mm yyyy</div>
-            <p>A paragraph summarising article <?php echo $postId;?></p>
+        <?php foreach($posts as $post): ?>
+            <h2><?php echo htmlspecialchars($post['title']); ?></h2>
+            <div><?php echo date($post['created_at']); ?></div>
+            <p><?php echo htmlspecialchars($post['body']);?></p>
             <p>
                 <a href="#">Read more...</a>
             </p>
-        <?php endfor;?>
+        <?php endforeach;?>
 
     </body>
 </html>
