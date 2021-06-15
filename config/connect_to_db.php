61dfebb92b52dc90d193611c470f85a24bbb75dd
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-06-15 13:10:15 +0545

    Config file to connect to database

---
+++ config/connect_to_db.php
@@ -0,0 +1,17 @@
+<?php
+
+    $servername = 'localhost';
+    $username = 'prabeg';
+    $password = 'test1234';
+    $database = 'blog';
+
+
+    // Create Connection
+    $conn = mysqli_connect($serverName, $username, $password, $database);
+
+    // Check Connection
+    if($conn->connect_error){
+        die("Conneciton failed: " . $conn->connect_error);
+    }
+
+?>
