c63831359ee2b2470b53c0198f41d4a4f6053701
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-06-15 13:49:03 +0545

    Add page for individual post

---
+++ view-post.php
@@ -0,0 +1,46 @@
+<?php
+    require('./config/connect_to_db.php');
+
+    if(isset($_GET['id'])){
+        $postId = mysqli_real_escape_string($conn, $_GET['id']);
+
+        // Statement to get post of particular id
+        $sql = "SELECT * FROM post WHERE id=$postId";
+
+        // Query the database to get the result
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            $post = mysqli_fetch_assoc($result);
+        } 
+        else {
+            echo "0 results";
+        }
+
+        // free $result
+        mysqli_free_result($result);
+
+        //close connection
+        mysqli_close($conn);
+    }
+?>
+
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>Blog title</h1>
+        <p>This paragraph summarises what the blog is about.</p>
+
+        <h2>
+            <?php echo htmlspecialchars($post['title']); ?>
+        </h2>
+        <div>
+            <?php echo date($post['created_at']); ?>
+        </div>
+        <p>
+            <?php echo htmlspecialchars($post['body']); ?>
+        </p>
+    </body>
+</html>
