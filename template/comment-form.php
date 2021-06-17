84a675c1b819777627fe988a7eb28182ae052535
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-06-17 17:26:30 +0545

    Add template comment form

---
+++ template/comment-form.php
@@ -0,0 +1,20 @@
+<h3>Add Comment</h3>
+
+<form action="../view-post.php" method="POST">
+    <p>
+        <label for="name">Name: </label>
+        <input type="text" name="name" value="">
+    </p>
+    
+    <p>
+        <label for="email">Email: </label>
+        <input type="email" name="email" value="">
+    </p>
+
+    <p>
+        <label for="comment">Comment: </label>
+        <textarea name="comment" id="" cols="70" rows="8"></textarea>
+    </p>
+    
+    <input type="submit" name="submit" value="submit">
+</form>
